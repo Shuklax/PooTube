60b7b1f2a97e807276f4e8ab39aa973c52e477f0
Shuklax <harshshuklaknp@gmail.com>
2024-12-11 17:21:54 +0530

    conclude the header

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,29 +1,29 @@
 const Head = () => {
   return (
-    <div className="grid grid-flow-col p-4 shadow-lg">
+    <div className="grid grid-flow-col p-4 shadow-xl">
       <div className="flex col-span-1">
         <img
-          className="h-8"
+          className="h-8 cursor-pointer"
           src="/hamburger-menu-filled-icon-in-transparent-background.jpg"
           alt="menu_icon"
         />
         <img
-          className="h-8 w-auto ps-2"
+          className="h-8 w-auto ps-2 cursor-pointer"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/YouTube_Logo_%282013-2017%29.svg/2560px-YouTube_Logo_%282013-2017%29.svg.png"
           alt="youtube_icon"
         />
       </div>
       <div className="col-span-10 text-center">
         <input
-          className="border bg-black border-gray-400 rounded-l-full px-4 w-1/2 overflow-hidden h-9"
+          className="border bg-black text-gray-200 outline-none border-gray-800 rounded-l-full px-4 w-1/2 overflow-hidden h-9"
           type="text"
           placeholder="Search..."
         />
-        <button className="bg-gray-700 text-gray-300 px-2 rounded-r-full h-9">
-          Search
+        <button className="bg-gray-800 px-2 rounded-r-full h-9 w-11 cursor-pointer">
+          🔍
         </button>
       </div>
-      <div className="col-span-1 h-8 flex flex-row-reverse">
+      <div className="col-span-1 h-8 flex flex-row-reverse cursor-pointer bg-white">
         <img src="/user-profile-icon.jpg" alt="user_profile" />
       </div>
     </div>
